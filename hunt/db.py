562b93ffe483c50728eaa6fa36f0b68b291bb166
apple1417 <git@apple1417.dev>
2024-01-13 23:20:46 +1300

    move expandable balances to db
    required removing the cached connection, since they're accessed on different threads, which is not supported

--- hunt/db.py
+++ hunt/db.py
@@ -10,9 +10,6 @@ from mods_base import SETTINGS_DIR, open_in_mod_dir
 DB_PATH = SETTINGS_DIR / "hunt.sqlite3"
 DB_TEMPLATE_PATH = Path(__file__).parent / "generate_db" / "hunt.sqlite3"
 
-cached_readonly_con: sqlite3.Connection | None = None
-cached_readwrite_con: sqlite3.Connection | None = None
-
 
 @contextmanager
 def open_db(mode: Literal["r", "w"]) -> Iterator[sqlite3.Cursor]:
@@ -24,44 +21,28 @@ def open_db(mode: Literal["r", "w"]) -> Iterator[sqlite3.Cursor]:
     Returns:
         A new cursor for the db.
     """
-    global cached_readonly_con, cached_readwrite_con
-
     if not DB_PATH.exists():
         reset_db()
 
-    if cached_readonly_con is None or cached_readwrite_con is None:
-        cached_readonly_con = sqlite3.connect(f"file:{DB_PATH}?mode=ro", uri=True)
-        cached_readwrite_con = sqlite3.connect(f"file:{DB_PATH}", uri=True)
-
-        cached_readonly_con.cursor().execute("PRAGMA foreign_keys = ON")
-        cached_readwrite_con.cursor().execute("PRAGMA foreign_keys = ON")
-
+    read_only = "" if mode == "w" else "?mode=ro"
+    con = sqlite3.connect(f"file:{DB_PATH}{read_only}", uri=True)
     if mode == "w":
-        cur = cached_readwrite_con.cursor()
+        cur = con.cursor()
 
         try:
             yield cur
-            cached_readwrite_con.commit()
+            con.commit()
         except Exception:  # noqa: BLE001
-            cached_readwrite_con.rollback()
+            con.rollback()
         finally:
             cur.close()
 
     else:
-        yield cached_readonly_con.cursor()
+        yield con.cursor()
 
 
 def reset_db() -> None:
     """Resets the db back to default."""
-    global cached_readonly_con, cached_readwrite_con
-
-    if cached_readonly_con is not None:
-        cached_readonly_con.close()
-    cached_readonly_con = None
-    if cached_readwrite_con is not None:
-        cached_readwrite_con.close()
-    cached_readwrite_con = None
-
     DB_PATH.unlink(missing_ok=True)
     with open_in_mod_dir(DB_TEMPLATE_PATH, binary=True) as template, DB_PATH.open("wb") as db:
         shutil.copyfileobj(template, db)
--- hunt/generate_db/generate.py
+++ hunt/generate_db/generate.py
@@ -430,6 +430,204 @@ TRUE_TRIAL_RESTRICTION_NAMES: tuple[tuple[str, str], ...] = (
     ("Fasterfied Tizzy / Moar Fasterfied Tizzy", "Tyrant of Instinct"),
 )
 
+EXPANDABLE_BALANCE_DATA: dict[str, dict[str, str]] = {
+    (
+        "/Game/Gear/Artifacts/_Design/BalanceDefs/InvBalD_Artifact_05_Legendary.InvBalD_Artifact_05_Legendary"
+    ): {
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Slide/SplatterGun/Artifact_Part_Ability_SplatterGun.Artifact_Part_Ability_SplatterGun"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/SplatterGun/InvBalD_Artifact_SplatterGun.InvBalD_Artifact_SplatterGun"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Misc/MoxxisEndowment/Artifact_Part_Ability_MoxxisEndowment.Artifact_Part_Ability_MoxxisEndowment"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/MoxxisEndowment/InvBalD_Artifact_MoxxisEndowment.InvBalD_Artifact_MoxxisEndowment"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Misc/OttoIdol/Artifact_Part_Ability_OttoIdol.Artifact_Part_Ability_OttoIdol"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/OttoIdol/InvBalD_Artifact_OttoIdol.InvBalD_Artifact_OttoIdol"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Melee/CommanderPlanetoid/Artifact_Part_Ability_CommanderPlanetoid.Artifact_Part_Ability_CommanderPlanetoid"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/CommanderPlanetoid/InvBalD_Artifact_CommanderPlanetoid.InvBalD_Artifact_CommanderPlanetoid"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Slam/PullOutMethod/Artifact_Part_Ability_PullOutMethod.Artifact_Part_Ability_PullOutMethod"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/PullOutMethod/InvBalD_Artifact_PullOutMethod.InvBalD_Artifact_PullOutMethod"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Slide/StaticTouch/Artifact_Part_Ability_StaticTouch.Artifact_Part_Ability_StaticTouch"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/StaticTouch/InvBalD_Artifact_StaticTouch.InvBalD_Artifact_StaticTouch"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Misc/LoadedDice/Artifact_Part_Ability_LoadedDice.Artifact_Part_Ability_LoadedDice"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/LoadedDice/InvBalD_Artifact_LoadedDice.InvBalD_Artifact_LoadedDice"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Melee/WhiteElephant/Artifact_Part_Ability_WhiteElephant.Artifact_Part_Ability_WhiteElephant"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/WhiteElephant/InvBalD_Artifact_WhiteElephant.InvBalD_Artifact_WhiteElephant"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Slide/RocketBoots/Artifact_Part_Ability_RocketBoots.Artifact_Part_Ability_RocketBoots"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/RocketBoots/InvBalD_Artifact_RocketBoots.InvBalD_Artifact_RocketBoots"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Misc/Deathless/Artifact_Part_Ability_Deathless.Artifact_Part_Ability_Deathless"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/Deathless/InvBalD_Artifact_Deathless.InvBalD_Artifact_Deathless"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Misc/VictoryRush/Artifact_Part_Ability_VictoryRush.Artifact_Part_Ability_VictoryRush"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/VictoryRush/InvBalD_Artifact_VictoryRush.InvBalD_Artifact_VictoryRush"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Slam/CosmicCrater/Artifact_Part_Ability_CosmicCrater.Artifact_Part_Ability_CosmicCrater"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/CosmicCrater/InvBalD_Artifact_CosmicCrater.InvBalD_Artifact_CosmicCrater"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Slam/Salvo/Artifact_Part_Ability_Salvo.Artifact_Part_Ability_Salvo"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/Salvo/InvBalD_Artifact_Salvo.InvBalD_Artifact_Salvo"
+        ),
+        (
+            "/Game/Gear/Artifacts/_Design/PartSets/Abilities/_Legendary/Slam/Safegaurd/Artifact_Part_Ability_Safegaurd.Artifact_Part_Ability_Safegaurd"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/Artifacts/Safegaurd/InvBalD_Artifact_Safegaurd.InvBalD_Artifact_Safegaurd"
+        ),
+    },
+    (
+        "/Game/Gear/ClassMods/_Design/BalanceDefs/InvBalD_ClassMod_Beastmaster_05_Legendary.InvBalD_ClassMod_Beastmaster_05_Legendary"
+    ): {
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Beastmaster/_Unique/Beastmaster_Unique_01/ClassMod_Part_Beastmaster_Unique_01_Friendbot.ClassMod_Part_Beastmaster_Unique_01_Friendbot"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/BeastMaster/InvBalD_ClassMod_Beastmaster_FriendBot.InvBalD_ClassMod_Beastmaster_FriendBot"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Beastmaster/_Unique/Beastmaster_Unique_02/ClassMod_Part_Beastmaster_Unique_02_CosmicStalker.ClassMod_Part_Beastmaster_Unique_02_CosmicStalker"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/BeastMaster/InvBalD_ClassMod_Beastmaster_CosmicStalker.InvBalD_ClassMod_Beastmaster_CosmicStalker"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Beastmaster/_Unique/Beastmaster_Unique_03/ClassMod_Part_Beastmaster_Unique_03_BountyHunter.ClassMod_Part_Beastmaster_Unique_03_BountyHunter"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/BeastMaster/InvBalD_ClassMod_Beastmaster_BountyHunter.InvBalD_ClassMod_Beastmaster_BountyHunter"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Beastmaster/_Unique/Beastmaster_Unique_04/ClassMod_Part_Beastmaster_Unique_04_DE4DEYE.ClassMod_Part_Beastmaster_Unique_04_DE4DEYE"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/BeastMaster/InvBalD_ClassMod_Beastmaster_DE4DEYE.InvBalD_ClassMod_Beastmaster_DE4DEYE"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Beastmaster/_Unique/Beastmaster_Unique_05/ClassMod_Part_Beastmaster_Unique_05_RakkCommander.ClassMod_Part_Beastmaster_Unique_05_RakkCommander"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/BeastMaster/InvBalD_ClassMod_Beastmaster_RakkCommander.InvBalD_ClassMod_Beastmaster_RakkCommander"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Beastmaster/_Unique/Beastmaster_Unique_06/ClassMod_Part_Beastmaster_Unique_06_RedFang.ClassMod_Part_Beastmaster_Unique_06_RedFang"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/BeastMaster/InvBalD_ClassMod_Beastmaster_RedFang.InvBalD_ClassMod_Beastmaster_RedFang"
+        ),
+    },
+    (
+        "/Game/Gear/ClassMods/_Design/BalanceDefs/InvBalD_ClassMod_Gunner_05_Legendary.InvBalD_ClassMod_Gunner_05_Legendary"
+    ): {
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Gunner/_Unique/Unique_01/ClassMod_Part_Gunner_Unique_01_Rocketeer.ClassMod_Part_Gunner_Unique_01_Rocketeer"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Gunner/InvBalD_ClassMod_Gunner_Rocketeer.InvBalD_ClassMod_Gunner_Rocketeer"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Gunner/_Unique/Unique_02/ClassMod_Part_Gunner_Unique_02_ColdBlooded.ClassMod_Part_Gunner_Unique_02_ColdBlooded"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Gunner/InvBalD_ClassMod_Gunner_BloodLetter.InvBalD_ClassMod_Gunner_BloodLetter"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Gunner/_Unique/Unique_03/ClassMod_Part_Gunner_Unique_03.ClassMod_Part_Gunner_Unique_03"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Gunner/InvBalD_ClassMod_Gunner_MindSweeper.InvBalD_ClassMod_Gunner_MindSweeper"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Gunner/_Unique/Unique_04/ClassMod_Part_Gunner_Unique_04.ClassMod_Part_Gunner_Unique_04"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Gunner/InvBalD_ClassMod_Gunner_BearTrooper.InvBalD_ClassMod_Gunner_BearTrooper"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Gunner/_Unique/Unique_05/ClassMod_Part_Gunner_Unique_05.ClassMod_Part_Gunner_Unique_05"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Gunner/InvBalD_ClassMod_Gunner_BlastMaster.InvBalD_ClassMod_Gunner_BlastMaster"
+        ),
+    },
+    (
+        "/Game/Gear/ClassMods/_Design/BalanceDefs/InvBalD_ClassMod_Operative_05_Legendary.InvBalD_ClassMod_Operative_05_Legendary"
+    ): {
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Operative/_Unique/Unique_01/ClassMod_Part_Operative_Unique_01_Infiltrator.ClassMod_Part_Operative_Unique_01_Infiltrator"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Operative/InvBalD_ClassMod_Operative_Infiltrator.InvBalD_ClassMod_Operative_Infiltrator"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Operative/_Unique/Unique_02/ClassMod_Part_Operative_Unique_02_Executor.ClassMod_Part_Operative_Unique_02_Executor"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Operative/InvBalD_ClassMod_Operative_Executor.InvBalD_ClassMod_Operative_Executor"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Operative/_Unique/Unique_03/ClassMod_Part_Operative_Unique_03_Expert.ClassMod_Part_Operative_Unique_03_Expert"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Operative/InvBalD_ClassMod_Operative_Techspert.InvBalD_ClassMod_Operative_Techspert"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Operative/_Unique/Unique_04/ClassMod_Part_Operative_Unique_04_Firebrand.ClassMod_Part_Operative_Unique_04_Firebrand"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Operative/InvBalD_ClassMod_Operative_FireBrand.InvBalD_ClassMod_Operative_FireBrand"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/Operative/_Unique/Unique_05/ClassMod_Part_Operative_Unique_05_Operative.ClassMod_Part_Operative_Unique_05_Operative"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Operative/InvBalD_ClassMod_Operative_ColdWarrior.InvBalD_ClassMod_Operative_ColdWarrior"
+        ),
+    },
+    (
+        "/Game/Gear/ClassMods/_Design/BalanceDefs/InvBalD_ClassMod_Siren_05_Legendary.InvBalD_ClassMod_Siren_05_Legendary"
+    ): {
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/SIren/_Unique/Unique_01/ClassMod_Part_Siren_Unique_01_Storm.ClassMod_Part_Siren_Unique_01_Storm"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Siren/InvBalD_ClassMod_Siren_Elementalist.InvBalD_ClassMod_Siren_Elementalist"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/SIren/_Unique/Unique_02/ClassMod_Part_Siren_Unique_02_Dragon.ClassMod_Part_Siren_Unique_02_Dragon"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Siren/InvBalD_ClassMod_Siren_Dragon.InvBalD_ClassMod_Siren_Dragon"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/SIren/_Unique/Unique_03/ClassMod_Part_Siren_Unique_03_Bruiser.ClassMod_Part_Siren_Unique_03_Bruiser"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Siren/InvBalD_ClassMod_Siren_Breaker.InvBalD_ClassMod_Siren_Breaker"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/SIren/_Unique/Unique_04/ClassMod_Part_Siren_Unique_04_Phasezerker.ClassMod_Part_Siren_Unique_04_Phasezerker"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Siren/InvBalD_ClassMod_Siren_Phasezerker.InvBalD_ClassMod_Siren_Phasezerker"
+        ),
+        (
+            "/Game/Gear/ClassMods/_Design/PartSets/Part_ClassMod/SIren/_Unique/Unique_05/ClassMod_Part_Siren_Unique_05_Siren.ClassMod_Part_Siren_Unique_05_Siren"
+        ): (
+            "/Game/PatchDLC/Raid1/Gear/ClassMods/Siren/InvBalD_ClassMod_Siren_Nimbus.InvBalD_ClassMod_Siren_Nimbus"
+        ),
+    },
+}
+
 
 def match_source_restriction(item_name: str, enemy_name: str) -> str | None:
     """
@@ -1231,6 +1429,30 @@ if __name__ == "__main__":
         )
         """,
     )
+
+    cur.execute(
+        """
+        CREATE TABLE ExpandableBalances (
+            ID              INTEGER NOT NULL UNIQUE,
+            RootBalance     TEXT NOT NULL,
+            Part            TEXT NOT NULL,
+            ExpandedBalance TEXT NOT NULL,
+            PRIMARY KEY(ID AUTOINCREMENT),
+            FOREIGN KEY(ExpandedBalance) REFERENCES Items(Balance)
+        )
+        """,
+    )
+    for root_balance, inner in EXPANDABLE_BALANCE_DATA.items():
+        for part, expanded_balance in inner.items():
+            cur.execute(
+                """
+                INSERT INTO
+                    ExpandableBalances (RootBalance, Part, ExpandedBalance)
+                VALUES
+                    (?, ?, ?)
+                """,
+                (root_balance, part, expanded_balance),
+            )
 
     cur.close()
     con.commit()
