27c04929185ae43010e6182a84ea8c1733fa8fa2
apple1417 <git@apple1417.dev>
2024-02-06 10:20:58 +1300

    convert on screen display to native module
    for a significant performance improvment

--- hunt/osd.py
+++ hunt/osd.py
@@ -1,17 +1,15 @@
 # ruff: noqa: D103
 
 import string
-import textwrap
 from collections.abc import Callable
 from dataclasses import KW_ONLY, dataclass
 from threading import Thread
 from typing import Any, Self
 
-import unrealsdk
-from mods_base import SETTINGS_DIR, BoolOption, GroupedOption, HookProtocol, hook
-from unrealsdk.unreal import BoundFunction, UObject, WrappedStruct
+from mods_base import SETTINGS_DIR, BoolOption, GroupedOption
 
 from .db import open_db
+from .native import osd
 
 OUTPUT_TEXT_FILE = SETTINGS_DIR / "hunt_osd.txt"
 TEMPLATE_TEXT_FILE = SETTINGS_DIR / "hunt_osd.template.txt"
@@ -325,40 +323,17 @@ def create_template_file() -> None:
         )
 
 
-WHITE = unrealsdk.make_struct("LinearColor", R=1, G=1, B=1, A=1)
-BLACK_50 = unrealsdk.make_struct("LinearColor", A=0.5)
-FONT = unrealsdk.find_object("Font", "/Game/UI/_Shared/Fonts/OAK_BODY.OAK_BODY")
-OUTER_PADDING = 10
-INTER_LINE_PADDING = 1
-
-DRAW_RECT = unrealsdk.find_object("Function", "/Script/Engine.HUD:DrawRect")
-DRAW_TEXT = unrealsdk.find_object("Function", "/Script/Engine.HUD:DrawText")
-
-# Since calculating what to draw each frame is expensive, we'll just construct a hook that draws
-# exactly what we need
-draw_osd_hook: HookProtocol | None = None
-
-# We format the lines to draw in a thread
-# However, for some reason, trying to call `HUD::GetTextSize` out of band just returns 0, so we
-# can't calculate layout in the thread - we use this list to transfer between the two
-lines_to_draw: list[str] = []
-
-
 def update_osd() -> None:
     # Updating is expensive, do it in a thread
     Thread(target=_update_osd_inner).start()
 
 
 def _update_osd_inner() -> None:
-    global draw_osd_hook, lines_to_draw
-
     # Can't put this any higher due to circular imports
     from . import mod
 
     if not mod.is_enabled:
-        if draw_osd_hook is not None:
-            draw_osd_hook.disable()
-            draw_osd_hook = None
+        osd.hide()
         return
 
     if not TEMPLATE_TEXT_FILE.exists():
@@ -369,91 +344,12 @@ def _update_osd_inner() -> None:
 
     # If nothing to draw
     if not any(stat.value for stat in ALL_STATS):
-        if draw_osd_hook is not None:
-            draw_osd_hook.disable()
-            draw_osd_hook = None
+        osd.hide()
         return
 
     lines_to_draw = [format_stats(stat.in_game_format) for stat in ALL_STATS if stat.value]
     if not lines_to_draw:
         return
 
-    update_osd_hook.enable()
-
-
-@hook("/Script/Engine.HUD:ReceiveDrawHUD")
-def update_osd_hook(
-    obj: UObject,
-    _2: WrappedStruct,
-    _3: Any,
-    _4: BoundFunction,
-) -> None:
-    global draw_osd_hook
-
-    update_osd_hook.disable()
-
-    if draw_osd_hook is not None:
-        draw_osd_hook.disable()
-    draw_osd_hook = None
-
-    max_width: float = 0
-    heights: list[float] = []
-    for line in lines_to_draw:
-        _, w, h = obj.GetTextSize(line, 0, 0, FONT, 1)
-        max_width = max(w, max_width)
-        heights.append(h)
-
-    source = textwrap.dedent(
-        f"""
-        @hook("/Script/Engine.HUD:ReceiveDrawHUD")
-        def draw_osd_hook(
-            obj: UObject,
-            _2: WrappedStruct,
-            _3: Any,
-            _4: BoundFunction,
-        ) -> None:
-            obj._get_field(DRAW_RECT)(
-                BLACK_50,
-                0,
-                0,
-                {max_width + 2 * OUTER_PADDING!r},
-                {sum(heights) + len(heights) * INTER_LINE_PADDING + 2 * OUTER_PADDING!r},
-            )
-        """,
-    )
-    for idx, line in enumerate(lines_to_draw):
-        source += textwrap.indent(
-            textwrap.dedent(
-                f"""
-                obj._get_field(DRAW_TEXT)(
-                    {line!r},
-                    WHITE,
-                    {OUTER_PADDING!r},
-                    {OUTER_PADDING + sum(heights[:idx]) + idx * INTER_LINE_PADDING!r},
-                    FONT,
-                    1,
-                    False,
-                )
-                """,
-            ),
-            prefix=" " * 4,
-        )
-
-    globals_dict: dict[str, Any] = {
-        "hook": hook,
-        "UObject": UObject,
-        "WrappedStruct": WrappedStruct,
-        "Any": Any,
-        "BoundFunction": BoundFunction,
-        "BLACK_50": BLACK_50,
-        "WHITE": WHITE,
-        "FONT": FONT,
-        "DRAW_RECT": DRAW_RECT,
-        "DRAW_TEXT": DRAW_TEXT,
-    }
-    exec(source, globals_dict)  # noqa: S102
-
-    new_hook: HookProtocol = globals_dict["draw_osd_hook"]
-
-    draw_osd_hook = new_hook
-    draw_osd_hook.enable()
+    osd.update_lines(lines_to_draw)
+    osd.show()
