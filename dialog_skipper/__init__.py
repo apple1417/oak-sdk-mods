47232de64346dd823afcd3783016de4e11368a7b
apple1417 <git@apple1417.dev>
2024-04-07 17:52:42 +1200

    add auto dialog skip and fast forward

--- dialog_skipper/__init__.py
+++ dialog_skipper/__init__.py
@@ -2,8 +2,11 @@
 if True:
     assert __import__("mods_base").__version_info__ >= (1, 0), "Please update the SDK"
 
+from typing import Any
+
 import unrealsdk
-from mods_base import build_mod, keybind
+from mods_base import ENGINE, BoolOption, EInputEvent, build_mod, hook, keybind
+from unrealsdk.hooks import Block, Type
 
 __version__: str
 __version_info__: tuple[int, ...]
@@ -15,6 +18,55 @@ def skip_dialog() -> None:
         thread_id = dialog.CurrentPerformance.DialogThreadID
         if thread_id > 0:
             dialog.StopPerformance(thread_id, True)
+
+
+auto_skip = BoolOption(
+    "Auto Skip",
+    False,
+    description=(
+        "Automatically skip all dialog as soon as it is started. This includes <i>all dialog</i>,"
+        " including miscellaneous idle dialog, so you might want to disable this while not playing"
+        " through the story."
+    ),
+)
+
+
+@hook("/Script/GbxDialog.GbxDialogComponent:StartPerformance", Type.PRE)
+def dialog_start_performance(*_: Any) -> None | type[Block]:
+    return Block if auto_skip.value else None
+
+
+# This is basically stolen straight from ABCD
+
+gameplay_statics = unrealsdk.find_class("GameplayStatics").ClassDefaultObject
+
+set_time_dialation = gameplay_statics.SetGlobalTimeDilation
+get_time_dialoation = gameplay_statics.GetGlobalTimeDilation
+
+FAST_FORWARD_SPEED = 8
+
+
+@keybind("Fast Forward (Toggle)")
+def fast_forward_toggle() -> None:
+    world = ENGINE.GameViewport.World
+    current_dialation = get_time_dialoation(world)
+    new_dialation = FAST_FORWARD_SPEED if current_dialation == 1 else 1
+    set_time_dialation(world, new_dialation)
+
+
+@keybind("Fast Forward (Hold)", event_filter=None)
+def fast_forward_hold(event: EInputEvent) -> None:
+    dialation: float
+
+    match event:
+        case EInputEvent.IE_Pressed:
+            dialation = FAST_FORWARD_SPEED
+        case EInputEvent.IE_Released:
+            dialation = 1
+        case _:
+            return
+
+    set_time_dialation(ENGINE.GameViewport.World, dialation)
 
 
 build_mod()
---
+++ dialog_skipper/changelog.md
@@ -0,0 +1,8 @@
+# Dialog Skipper - Changelog
+
+# v2
+- Added an option to try automatically skip dialog
+- Added Fast Forward keybinds, which can be used to speed up sections locked behind animations
+
+# v1
+- Initial Release
